466f7fdad1ba2110c70b367e0a4f817fa809f1ec
mod <mod64bits@gmail.com>
2025-05-05 07:18:54 -0300

    view clientes

--- app/apps/clientes/models.py
+++ app/apps/clientes/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from apps.base.models import BaseModel
 
 # Create your models here.
 
-class Cliente(models.Model):
+class Cliente(BaseModel):
     TIPO_DOCUMENTO_CHOICES = [
         ('CPF', 'CPF'),
         ('CNPJ', 'CNPJ'),
@@ -11,9 +12,9 @@ class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     telefone = models.CharField(max_length=15)
-    documento = models.CharField(max_length=14, unique=True)
+    documento = models.CharField(max_length=30)
     tipo_documento = models.CharField(max_length=4, choices=TIPO_DOCUMENTO_CHOICES)
-    data_criacao = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.nome
